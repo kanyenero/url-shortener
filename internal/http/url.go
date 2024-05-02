fcfc2de20745bfc8b60c47d190897600a01e97c0
Zakhar Khakhalin <khakhalinz@gmail.com>
2024-05-02 11:20:12 +0300

    http-обработчики

--- internal/http/url.go
+++ internal/http/url.go
@@ -1,7 +1,12 @@
 package http
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 	"url-shortener/internal/service"
 )
 
@@ -14,9 +19,57 @@ func CreateUrlHandler(urlService *service.UrlService) *UrlHandler {
 }
 
 func (handler *UrlHandler) Get(writer http.ResponseWriter, request *http.Request) {
+	if request.RequestURI == "/s/" {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	requestURIValid, err := regexp.MatchString(`/s/[A-z0-9]{8}$`, request.RequestURI)
+	if !requestURIValid {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("Invalid RequestURI")
+		return
+	}
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println(err.Error())
+		return
+	}
+
+	splittedRequestURI := strings.Split(request.RequestURI, "/")
+	hash := splittedRequestURI[len(splittedRequestURI)-1]
+
+	url, err := (*handler.urlService).GetUrl(hash)
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		log.Println(err.Error())
+		return
+	}
+
+	http.Redirect(writer, request, url, http.StatusFound)
+	fmt.Fprintf(writer, "status: %s; url: %s", strconv.Itoa(http.StatusFound), url)
 }
 
 func (handler *UrlHandler) Set(writer http.ResponseWriter, request *http.Request) {
+	if request.RequestURI == "/a/" {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	requestUrl := request.URL.Query().Get("url")
+	if requestUrl == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("Query parameter 'url' is missing")
+		return
+	}
+
+	hash, err := (*handler.urlService).SetUrl(requestUrl)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		log.Println(err.Error())
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprintf(writer, "status: %s; hash: %s", strconv.Itoa(http.StatusOK), hash)
 }
